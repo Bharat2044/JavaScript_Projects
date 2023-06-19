8eb92970fe598f3d683d5aeff36e52e0a2a98cd1
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-19 11:17:53 +0000

    code added

---
+++ 03_counterApp/script.js
@@ -0,0 +1,46 @@
+const decrementBtn = document.getElementById("decrementBtn");
+const incrementBtn = document.getElementById("incrementBtn");
+const resetBtn = document.getElementById("resetBtn");
+const displayValue = document.getElementById("displayValue");
+
+// for decrement button click
+decrementBtn.addEventListener("click", () => {
+  const value = Number(displayValue.innerText);
+  displayValue.innerText = value - 1;
+});
+
+// for increment button click
+incrementBtn.addEventListener("click", () => {
+  const value = Number(displayValue.innerText);  
+  displayValue.innerText = value + 1;
+});
+
+// for reset button click
+resetBtn.addEventListener("click", () => {
+  displayValue.innerText = 0;
+});
+
+// // for decrement button click
+// decrementBtn.addEventListener("click", () => {
+//     const value = Number(displayValue.innerText);
+//     if (value > 0) {
+//       displayValue.innerText = value - 1;
+//     } else {
+//       alert("Negative value not allowed");
+//     }
+//   });
+
+//   // for increment button click
+//   incrementBtn.addEventListener("click", () => {
+//     const value = Number(displayValue.innerText);
+//     if (value >= 10) {
+//       alert("10+ values are not allowed");
+//     } else {
+//       displayValue.innerText = value + 1;
+//     }
+//   });
+
+//   // for reset button click
+//   resetBtn.addEventListener("click", () => {
+//     displayValue.innerText = 0;
+//   });
